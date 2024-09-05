0769041bbc146bed9c810054f3b85cb3931b6b9b
Nathan Araújo <nathanaraujo@Nathans-MacBook-Pro.local>
2024-09-05 02:08:12 +0100

    Add docker-compose

--- run-migrations.js
+++ run-migrations.js
@@ -3,58 +3,54 @@ const fs = require('fs');
 const path = require('path');
 
 // MongoDB URI
-const uri = 'mongodb://localhost:27017/thecollegestore';
+const uri = process.env.MONGO_URL || 'mongodb://localhost:27017';
 
 // Define a function to run migrations
 async function runMigrations() {
   try {
     // Connect to MongoDB
-    await mongoose.connect(uri, {
+    await mongoose.connect(`${uri}/thecollegestore?`, {
       useNewUrlParser: true,
       useUnifiedTopology: true,
     });
     console.log('Connected to MongoDB');
 
-    // Path to migrations folder
-    const migrationsPath = path.join(__dirname, 'migrations');
-
-    // Read files from migrations folder
+     const migrationsPath = path.join(__dirname, 'migrations');
     const files = fs.readdirSync(migrationsPath);
+    
+    // Use a coleção 'migrations' para registrar quais migrações foram executadas
+    const migrationRecords = mongoose.connection.db.collection('migrations');
 
     for (const file of files) {
-      const filePath = path.join(migrationsPath, file);
-      try {
-        // Read migration file content
-        const migrationContent = fs.readFileSync(filePath, 'utf8');
+      const migrationName = path.basename(file, '.js');
+      const migrationExists = await migrationRecords.findOne({ name: migrationName });
 
-        // Create a new function to execute migration
-        const runMigration = new Function('db', 'ObjectId', migrationContent);
-
-        // Pass the database connection and ObjectId
-        await runMigration(mongoose.connection.db, mongoose.Types.ObjectId);
-
-        console.log(`Migration ${file} executed successfully`);
-      } catch (err) {
-        console.error(`Error executing migration ${file}:`, err);
-        throw err; // Propagate the error up
+      if (migrationExists) {
+        console.log(`Migration ${file} already executed.`);
+        continue;
       }
+
+      const filePath = path.join(migrationsPath, file);
+      const migrationContent = fs.readFileSync(filePath, 'utf8');
+      const runMigration = new Function('db', 'ObjectId', migrationContent);
+
+      await runMigration(mongoose.connection.db, mongoose.Types.ObjectId);
+
+      // Adiciona um registro indicando que a migração foi executada
+      await migrationRecords.insertOne({ name: migrationName });
+      console.log(`Migration ${file} executed successfully`);
     }
 
     console.log('All migrations executed successfully');
-
-    // Wait for 10 seconds before disconnecting to ensure all operations have completed
-    console.log('Waiting all migrations before disconnecting...');
     await new Promise(resolve => setTimeout(resolve, 10000));
 
   } catch (err) {
     console.error('Error during migrations:', err);
   } finally {
-    // Disconnect from MongoDB and close the process gracefully
     await mongoose.disconnect();
     console.log('Disconnected from MongoDB');
     process.exit(0);
   }
 }
 
-// Execute migrations
 runMigrations();
