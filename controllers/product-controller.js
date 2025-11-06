26d8a80947e1bfa43003fdce6ddc2f4f7e563b0f
Nathan Araújo <nathanaraujo@Mac.home>
2025-11-06 21:31:49 +0000

    add feature to favotires

--- controllers/product-controller.js
+++ controllers/product-controller.js
@@ -1,12 +1,12 @@
 
 import { Product , ProductV2} from "../models/product"
+import Favorite from "../models/favoriteModel";
 import multer, { diskStorage } from "multer";
 import { extname } from "path";
 
 
 // endpoint for getting all products data
 export async function getAllproducts(req, res) {
-// app.get("/allproducts", async (req, res) => {
     let products = await Product.find({});
     console.log("All Products");
     res.send(products);
@@ -15,7 +15,6 @@ export async function getAllproducts(req, res) {
   
   // endpoint for getting latest products data
   export async function getNewcollections(req, res) {
-//   app.get("/newcollections", async (req, res) => {
     let products = await Product.find({});
     let arr = products.slice(0).slice(-8);
     console.log("New Collections");
@@ -25,7 +24,6 @@ export async function getAllproducts(req, res) {
   
   // endpoint for getting women products data
   export async function getPopularinwomen(req, res) {
-//   app.get("/popularinwomen", async (req, res) => {
     let products = await Product.find({ category: "women" });
     let arr = products.splice(0, 4);
     console.log("Popular In Women");
@@ -35,7 +33,6 @@ export async function getAllproducts(req, res) {
   // endpoint for getting women products data
   
   export async function getRelatedProducts(req, res) {
-//   app.post("/relatedproducts", async (req, res) => {
     console.log("Related Products");
     const {category} = req.body;
     const products = await Product.find({ category });
@@ -47,7 +44,6 @@ export async function getAllproducts(req, res) {
   
 // Create an endpoint for adding products using admin panel
 export async function addProduct(req, res) {
-// app.post("/addproduct", async (req, res) => {
     let products = await Product.find({});
     let id;
     if (products.length > 0) {
@@ -73,7 +69,6 @@ export async function addProduct(req, res) {
   
   // Create an endpoint for removing products using admin panel
   export async function removeProduct(req, res) {
-//   app.post("/removeproduct", async (req, res) => {
     await Product.findOneAndDelete({ id: req.body.id });
     console.log("Removed");
     res.json({ success: true, name: req.body.name })
@@ -81,21 +76,16 @@ export async function addProduct(req, res) {
 
   
   export async function getProductsV2(req, res) {
-// app.get('/api/v2/products', async (req, res) => {
     try {
-      // Consulta os produtos com o filtro aplicado
       const products = await ProductV2.find();
   
-      // Retorna a lista de produtos encontrados
       res.json(products);
     } catch (error) {
-      // Retorna uma mensagem de erro se algo der errado
       res.status(500).json({ message: 'Error retrieving products', error });
     }
   };
   
   export async function getFavouriteProducts(req, res) {
-//   app.post('/api/v2/products/favourites', async (req, res) => {
     const productIds = req.body.productIds; 
   
     try {
@@ -107,7 +97,6 @@ export async function addProduct(req, res) {
   };
   
   export async function getProductsByCategory(req, res) {
-//   app.get('/api/v2/products/category', async (req, res) => {
     const { categoryId, limit } = req.query;
     const limitInt = parseInt(limit, 10) || 4;
 
@@ -122,7 +111,6 @@ export async function addProduct(req, res) {
   };
   
   export async function seachProduct(req, res) {
-//   app.get('/api/v1/products/search', async (req, res) => {
     try {
       const { query, categoryId, brandId, minPrice, maxPrice } = req.query;
   
@@ -168,9 +156,68 @@ const storage = diskStorage({
 const upload = multer({ storage: storage })
 
 export async function uploadProduct(req, res) {
-// app.post("/upload", upload.single('product'), (req, res) => {
   res.json({
     success: 1,
     image_url: `/images/${req.file.filename}`
   })
 }
+
+//endpoint to toggle favourite product
+export const toggleFavouriteProduct = async (req, res) => {
+  try {
+    const { userId, productId } = req.body;
+
+    if (!userId || !productId) {
+      return res.status(400).json({ message: "userId e productId are required." });
+    }
+
+    const productExists = await Product.findById(productId);
+    if (!productExists) {
+      return res.status(404).json({ message: "Product not found." });
+    }
+
+    let favorite = await Favorite.findOne({ userId });
+
+    if (!favorite) {
+      favorite = new Favorite({ userId, products: [productId] });
+      await favorite.save();
+      return res.status(201).json({ message: "Product added to favorites.", favorites: favorite });
+    }
+
+    const isFavourite = favorite.products.some(id => id.toString()  === productId);
+
+    if (isFavourite) {
+      favorite.products = favorite.products.filter(id => id.toString()  !== productId);
+    } else {
+      favorite.products.push(productId);
+    }
+
+    await favorite.save();
+
+    res.status(200).json({
+      message: isFavourite ? "Produto removed from favorites." : "Product added to favorites.",
+      favorites: favorite
+    });
+
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ message: "Error to update favorites.", error: error.message });
+  }
+};
+
+export const getUserFavorites = async (req, res) => {
+  try {
+    const { userId } = req.params;
+
+    const favorite = await Favorite.findOne({ userId }).populate("products");
+
+    if (!favorite) {
+      return res.status(404).json({ message: "No favorite product found for the user" });
+    }
+
+    res.status(200).json(favorite);
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ message: "Error to get favorites.", error: error.message });
+  }
+};
--- routes/products.js
+++ routes/products.js
@@ -1,5 +1,7 @@
 import express from "express";
-import { addProduct, getAllproducts, getFavouriteProducts, getNewcollections, getPopularinwomen, getProductsByCategory, getProductsV2, getRelatedProducts, removeProduct, seachProduct, uploadProduct } from "../controllers/product-controller";
+import { addProduct, getAllproducts, getFavouriteProducts, getNewcollections, getPopularinwomen, 
+    getProductsByCategory, getProductsV2, getRelatedProducts, removeProduct, seachProduct, 
+    uploadProduct, toggleFavouriteProduct, getUserFavorites } from "../controllers/product-controller";
 const router = express.Router();
 
 router.get('/allproducts', getAllproducts);
@@ -10,6 +12,8 @@ router.post('/addproduct', addProduct);
 router.post('/removeproduct', removeProduct);
 router.get('/v2/products', getProductsV2);
 router.post('/v2/products/favourites', getFavouriteProducts);
+router.post('/v2/favorites/toggle', toggleFavouriteProduct);
+router.get('/v2/favorites/:userId', getUserFavorites);
 router.get('/v2/products/category', getProductsByCategory);
 router.get('/v1/products/search', seachProduct);
 router.get('/upload', uploadProduct);
